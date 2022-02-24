b2bfed9ef2b20146f584392db5477fd0a01b7d08
ISampol <99891169+ISampol@users.noreply.github.com>
2022-02-24 15:24:04 +0100

    Update java1.java

--- java1.java
+++ java1.java
@@ -6,5 +6,6 @@ public class java1 {
     public static void main(String[] args) {
         System.out.println("hols");
         System.out.println("Bones");
+        System.out.println("Hola24");
     }
 }
